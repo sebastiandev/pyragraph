d661ecef9d00b0bb83bb673ea73c53d4ed51750b
Sebastian <superpacko@gmail.com>
2016-03-02 11:25:36 -0300

    added filtering capabilities to the mongo iterator, to iterate only onthe filtered docs

--- sources/mongo_iterator.py
+++ sources/mongo_iterator.py
@@ -4,10 +4,11 @@ from pymongo import MongoClient
 
 class MongoIterator(object):
 
-    def __init__(self, uri, db, collection, skip=0, limit=0):
+    def __init__(self, uri, db, collection, skip=0, limit=0, filter=None):
         self._collection = MongoClient(uri)[db][collection]
         self._skip = skip
         self._limit = limit
+        self._filter = filter
 
     def __iter__(self):
         return self.stream()
@@ -18,8 +19,16 @@ class MongoIterator(object):
         if proj:
             proj.update({'_id': False})  # skip internal id
 
-        return self._collection.find(conditions, proj or None, skip=skip or self._skip, limit=limit or self._limit)
+        return self._collection.find(conditions or self._filter, proj or None, skip=skip or self._skip, limit=limit or self._limit)
 
     def size(self):
-        return self._collection.count()
+        return self._collection.count() if not self._filter else self._collection.find(self._filter).count()
+
+    @property
+    def filter(self):
+        return self._filter
+
+    @filter.setter
+    def filter(self, conditions):
+        self._filter = conditions
 
